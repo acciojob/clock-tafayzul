287ec984af7b462f161929729af9884f6a269f9a
tafayzul <107237505+tafayzul@users.noreply.github.com>
2026-02-03 12:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+// 'Id' must have a capital 'B'
+const p = document.getElementById("timer");
+
+setInterval(() => {
+    const date = new Date();
+   
+    p.textContent = date.toLocaleString(); 
+}, 1000);
